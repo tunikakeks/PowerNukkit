258b63d14fe6eeb474d0db8948b120d3753177ef
joserobjr <jose.rob.jr@gmail.com>
2020-02-10 23:51:23 -0300

    Start working on the writable_book implementation

--- src/main/java/cn/nukkit/network/protocol/BookEditPacket.java
+++ src/main/java/cn/nukkit/network/protocol/BookEditPacket.java
@@ -6,9 +6,27 @@ import lombok.ToString;
 @ToString
 public class BookEditPacket extends DataPacket {
 
+    public static final int NETWORK_ID = ProtocolInfo.BOOK_EDIT_PACKET;
+
+    public static final int TYPE_REPLACE_PAGE = 0;
+    public static final int TYPE_ADD_PAGE = 1;
+    public static final int TYPE_DELETE_PAGE = 2;
+    public static final int TYPE_SWAP_PAGES = 3;
+    public static final int TYPE_SIGN_BOOK = 4;
+
+    public int type;
+    public int inventorySlot;
+    public int pageNumber;
+    public int secondaryPageNumber;
+    public String text;
+    public String photoName;
+    public String title;
+    public String author;
+    public String xUID;
+
     @Override
     public short pid() {
-        return ProtocolInfo.BOOK_EDIT_PACKET;
+        return NETWORK_ID;
     }
 
     @Override

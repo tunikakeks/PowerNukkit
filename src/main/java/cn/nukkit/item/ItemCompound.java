e483472d8cbbd571c00d513bac94d9c8c0b3f264
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-26 15:41:44 +0900

    Add More Compound Names

--- src/main/java/cn/nukkit/item/ItemCompound.java
+++ src/main/java/cn/nukkit/item/ItemCompound.java
@@ -26,7 +26,7 @@ public class ItemCompound extends Item {
     protected static String getName(int meta) {
         switch (meta) {
             case 0:
-                return "Salt"
+                return "Salt";
             case 1:
                 return "Sodium Oxide";
             case 2:
@@ -35,6 +35,50 @@ public class ItemCompound extends Item {
                 return "Magnesium Nitrate";
             case 4:
                 return "Iron Sulfide";
+            case 5:
+                return "Lithium Hydride";
+            case 6:
+                return "Sodium Hydride";
+            case 7:
+                return "Calcium Bromide";
+            case 8:
+                return "Magnesium Oxide";
+            case 9:
+                return "Sodium Acetate";
+            case 10:
+                return "Luminol";
+            case 11:
+                return "Charcoal"; // Invalid Natural Compound
+            case 12:
+                return "Suger"; // Invalid Natural Compound
+            case 13:
+                return "Aluminium Oxide";
+            case 14:
+                return "Boron Trioxide";
+            case 15:
+                return ""; // ?
+            case 16:
+                return "Polyethylene";
+            case 17:
+                return "Garbage";
+            case 18:
+                return "Magnesium Salts"; // ?
+            case 19:
+                return "Sulfate";
+            case 20:
+                return "Barium Sulfate";
+            case 21:
+                return "Potassium Chloride";
+            case 22:
+                return "Mercuric Chloride";
+            case 23:
+                return "Cerium Chloride";
+            case 24:
+                return "Tungsten Chloride";
+            case 25:
+                return "Calcium Chloride";
+            case 26:
+                return "Water";
             default:
                 return "Compound";
         }

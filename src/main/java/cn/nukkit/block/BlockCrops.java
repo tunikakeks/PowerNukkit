ccec3ac2672973397fcba2456a96fd3f8b5b80b7
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-27 08:13:03 +0900

    fix

--- src/main/java/cn/nukkit/block/BlockCrops.java
+++ src/main/java/cn/nukkit/block/BlockCrops.java
@@ -61,7 +61,7 @@ public abstract class BlockCrops extends BlockFlowable {
                 if (player != null && (player.gamemode & 0x01) == 0) {
                     item.count--;
                 }
-            } else if (item.getId() == Item.DYE && item.getDamage() == 0x0f) { // Super Fertilizer
+            } else if (item.getId() == Item.RAPID_FERTILIZER) { // Super Fertilizer
             if (this.getDamage() < 7) {
                 BlockCrops block = (BlockCrops) this.clone();
                 block.setDamage(7);

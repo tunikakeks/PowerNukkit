d5da736dde6f1964c3543b755291ef78076afd9e
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-26 13:13:11 +0900

    Create ItemSuperFertilizer.java

---
+++ src/main/java/cn/nukkit/ItemSuperFertilizer.java
@@ -0,0 +1,16 @@
+package cn.nukkit.item;
+
+public class ItemSuperFertilizer extends Item {
+
+    public ItemSuperFertilizer() {
+        this(0, 1);
+    }
+    
+    public ItemSuperFertilizer(Integer meta) {
+        this(meta, 1);
+    }
+    
+    public ItemSuperFertilizer(Integer meta, int count) {
+        super(RAPID_FERTILIZER, meta, count, "Super Fertilizer");
+    }
+}

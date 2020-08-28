7ea484395b0e2b039fa885b5874073b6616b3eaa
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-28 14:54:56 +0900

    Add AllowUnderwater NBT tag

--- src/main/java/cn/nukkit/entity/item/EntityPrimedTNT.java
+++ src/main/java/cn/nukkit/entity/item/EntityPrimedTNT.java
@@ -54,8 +54,9 @@ public class EntityPrimedTNT extends Entity implements EntityExplosive {
         return false;
     }
 
+    protected boolean allowUnderwater;
     protected int fuse;
-
+    
     protected Entity source;
 
     public EntityPrimedTNT(FullChunk chunk, CompoundTag nbt) {
@@ -85,7 +86,12 @@ public class EntityPrimedTNT extends Entity implements EntityExplosive {
         } else {
             fuse = 80;
         }
-
+        if (this.namedTag.contains("AllowUnderwater")) {
+            this.allowUnderwater = this.namedTag.getBoolean("AllowUnderwater");
+        } else {
+            this.allowUnderwater = false;
+        }
+        
         this.setDataFlag(DATA_FLAGS, DATA_FLAG_IGNITED, true);
         this.setDataProperty(new IntEntityData(DATA_FUSE_LENGTH, fuse));
 
@@ -100,6 +106,7 @@ public class EntityPrimedTNT extends Entity implements EntityExplosive {
     public void saveNBT() {
         super.saveNBT();
         namedTag.putByte("Fuse", fuse);
+        namedTag.putBoolean("AllowUnderwater", this.allowUnderwater);
     }
 
     public boolean onUpdate(int currentTick) {

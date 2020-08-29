5d9754bc23357d7063758daf3f1ca6a9e6074bad
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-29 13:55:45 +0900

    Make Faceable

--- src/main/java/cn/nukkit/block/BlockChemistryTable.java
+++ src/main/java/cn/nukkit/block/BlockChemistryTable.java
@@ -9,13 +9,14 @@ import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.math.BlockFace;
 import cn.nukkit.utils.BlockColor;
+import cn.nukkit.utils.Faceable;
 
 import static cn.nukkit.blockproperty.CommonBlockProperties.DIRECTION;
 
 /**
  * @author good777LUCKY
  */
-public class BlockChemistryTable extends BlockSolidMeta {
+public class BlockChemistryTable extends BlockSolidMeta implements Faceable {
     public static final BlockProperty<ChemistryTableType> CHEMISTRY_TABLE_TYPE = new ArrayBlockProperty<>(
         "chemistry_table_type", true, ChemistryTableType.class
     );
@@ -89,6 +90,16 @@ public class BlockChemistryTable extends BlockSolidMeta {
     
     public void setChemistryTableType(ChemistryTableType type) {
         setPropertyValue(CHEMISTRY_TABLE_TYPE, type);
+    }
+    
+    @Override
+    public BlockFace getBlockFace() {
+        return getPropertyValue(DIRECTION);
+    }
+    
+    @Override
+    public void setBlockFace(BlockFace face) {
+        setPropertyValue(DIRECTION, face);
     }
     
     @Override

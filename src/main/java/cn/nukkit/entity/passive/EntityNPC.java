3db6dc45163e3affce5a7685af442fa85333d87e
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-08 18:08:08 +0900

    Add NPC

--- src/main/java/cn/nukkit/entity/passive/EntityNPC.java
+++ src/main/java/cn/nukkit/entity/passive/EntityNPC.java
@@ -3,7 +3,43 @@ package cn.nukkit.entity.passive;
 /**
  * @author Pub4Game
  * @since 21.06.2016
+ *
+ * @author good777LUCKY
  */
-public interface EntityNPC {
+public interface EntityNPC extends EntityLiving {
 
+    public static final int NETWORK_ID = 51;
+    
+    public EntityNpc(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+    
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+    
+    @Override
+    public float getWidth() {
+        return 0.6f;
+    }
+    
+    @Override
+    public float getHeight() {
+        return 2.1f;
+    }
+    
+    @Override
+    public String getName() {
+        return "NPC";
+    }
+    
+    @Override
+    public void initEntity() {
+        super.initEntity();
+        this.setMaxHealth(Float.MAX_VALUE);
+        this.setHealth(20);
+        this.setNameTagVisible(true);
+        this.setNameTagAlwaysVisible(true);
+    }
 }

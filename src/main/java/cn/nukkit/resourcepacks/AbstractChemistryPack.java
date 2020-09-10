5d79ef5d1705ba904f6464395113ee39fd14b175
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-10 17:08:53 +0900

    Update AbstractChemistryPack.java

--- src/main/java/cn/nukkit/resourcepacks/AbstractChemistryPack.java
+++ src/main/java/cn/nukkit/resourcepacks/AbstractChemistryPack.java
@@ -5,18 +5,23 @@ public abstract class AbstractChemistryPack implements ResourcePack {
     private static final byte[] EMPTY_DATA = new byte[0];
     
     @Override
-    public String getPackName() {
-        return "Chemistry";
-    }
-    
-    @Override
     public byte[] getPackChunk(int off, int len) {
         return EMPTY_DATA;
     }
     
     @Override
+    public String getPackName() {
+        return "Chemistry";
+    }
+    
+    @Override
     public int getPackSize() {
         return 0;
+    }
+    
+    @Override
+    public String getPackVersion() {
+        return "1.0.0";
     }
     
     @Override

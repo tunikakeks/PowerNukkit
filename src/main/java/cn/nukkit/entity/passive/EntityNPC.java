20a04f68a3d881b085a77669b98fa616246feac4
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-10 16:20:37 +0900

    Revert

--- src/main/java/cn/nukkit/entity/passive/EntityNPC.java
+++ src/main/java/cn/nukkit/entity/passive/EntityNPC.java
@@ -1,60 +1,9 @@
 package cn.nukkit.entity.passive;
 
-import cn.nukkit.entity.EntityInteractable;
-import cn.nukkit.entity.EntityLiving;
-import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.nbt.tag.CompoundTag;
-
 /**
  * @author Pub4Game
  * @since 21.06.2016
- *
- * @author good777LUCKY
  */
-public class EntityNPC extends EntityLiving implements EntityInteractable {
+public interface EntityNPC {
 
-    public static final int NETWORK_ID = 51;
-    
-    public EntityNPC(FullChunk chunk, CompoundTag nbt) {
-        super(chunk, nbt);
-    }
-    
-    @Override
-    public int getNetworkId() {
-        return NETWORK_ID;
-    }
-    
-    @Override
-    public float getWidth() {
-        return 0.6f;
-    }
-    
-    @Override
-    public float getHeight() {
-        return 2.1f;
-    }
-    
-    @Override
-    public boolean canDoInteraction() {
-        return true;
-    }
-    
-    @Override
-    public String getInteractButtonText() {
-        return "action.interact.edit";
-    }
-    
-    @Override
-    public String getName() {
-        return "NPC";
-    }
-    
-    @Override
-    public void initEntity() {
-        super.initEntity();
-        this.setMaxHealth(Float.MAX_VALUE);
-        this.setHealth(20);
-        this.setNameTagVisible(true);
-        this.setNameTagAlwaysVisible(true);
-    }
 }

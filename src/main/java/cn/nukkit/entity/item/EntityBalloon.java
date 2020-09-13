e1e7fffd648c0722fec3d9f5392e58a7f97431c9
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-13 21:56:28 +0900

    Update EntityBalloon.java

--- src/main/java/cn/nukkit/entity/item/EntityBalloon.java
+++ src/main/java/cn/nukkit/entity/item/EntityBalloon.java
@@ -37,7 +37,7 @@ public class EntityBalloon extends Entity {
         }
         if (this.namedTag.contains("balloon_attached")) {
             this.balloonAttached = this.namedTag.getLong("balloon_attached");
-            this.dataProperties.putLong(DATA_LEAD_HOLDER_EID, this.balloonAttached);
+            this.dataProperties.putLong(DATA_BALLOON_ATTACHED_ENTITY, this.balloonAttached); // TODO: Or DATA_LEAD_HOLDER_EID?
             this.setDataFlag(DATA_FLAGS, DATA_FLAG_LEASHED, true);
         } else {
             this.balloonAttached = -1L; // Not Attached

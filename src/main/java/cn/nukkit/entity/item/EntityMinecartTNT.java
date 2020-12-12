aa93e843d51e7d7ebfc6aa8656929349af906e15
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-12 11:01:34 +0900

    Fix

--- src/main/java/cn/nukkit/entity/item/EntityMinecartTNT.java
+++ src/main/java/cn/nukkit/entity/item/EntityMinecartTNT.java
@@ -9,7 +9,7 @@ import cn.nukkit.entity.EntityExplosive;
 import cn.nukkit.entity.data.IntEntityData;
 import cn.nukkit.event.entity.EntityExplosionPrimeEvent;
 import cn.nukkit.item.Item;
-import cn.nukkit.item.ItemID;
+import cn.nukkit.item.ItemBlock;
 import cn.nukkit.level.Explosion;
 import cn.nukkit.level.GameRule;
 import cn.nukkit.level.Sound;
@@ -115,7 +115,7 @@ public class EntityMinecartTNT extends EntityMinecartAbstract implements EntityE
     public void dropItem() {
         super.dropItem();
         
-        this.level.dropItem(this, Item.get(ItemID.TNT));
+        this.level.dropItem(this, new ItemBlock(Block.get(BlockID.TNT)));
     }
 
     @Override

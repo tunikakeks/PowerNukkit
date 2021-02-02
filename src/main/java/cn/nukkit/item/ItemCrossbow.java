46259190db23fcfe8a56f830e017391e20aa0d9c
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-02-02 19:29:40 +0900

    Create ItemCrossbow.java

---
+++ src/main/java/cn/nukkit/item/ItemCrossbow.java
@@ -0,0 +1,49 @@
+package cn.nukkit.item;
+
+import cn.nukkit.Player;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.projectile.EntityArrow;
+import cn.nukkit.entity.projectile.EntityProjectile;
+import cn.nukkit.event.entity.EntityShootBowEvent;
+import cn.nukkit.event.entity.ProjectileLaunchEvent;
+import cn.nukkit.inventory.Inventory;
+import cn.nukkit.item.enchantment.Enchantment;
+import cn.nukkit.level.Sound;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.DoubleTag;
+import cn.nukkit.nbt.tag.FloatTag;
+import cn.nukkit.nbt.tag.ListTag;
+
+/**
+ * @author GoodLucky777
+ */
+public class ItemCrossbow extends ItemTool {
+
+    public ItemCrossbow() {
+        this(0, 1);
+    }
+    
+    public ItemCrossbow(Integer meta) {
+        this(meta, 1);
+    }
+    
+    public ItemCrossbow(Integer meta, int count) {
+        super(CROSSBOW, meta, count, "Crossbow");
+    }
+    
+    @Override
+    public int getMaxDurability() {
+        return ItemTool.DURABILITY_CROSSBOW;
+    }
+    
+    @Override
+    public boolean onClickAir(Player player, Vector3 directionVector) {
+        return player.isCreative();
+    }
+    
+    @Override
+    public boolean onRelease(Player player, int ticksUsed) {
+        return true;
+    }
+}

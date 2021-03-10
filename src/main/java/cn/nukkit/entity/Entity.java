6be4b9e7b10d6b49d5ea1eb5ceae8cfd0aa5e2d8
Superzock3003 <superzock3003.buisnes@gmail.com>
2021-03-10 20:13:12 +0100

    update to 1.16.210

--- src/main/java/cn/nukkit/entity/Entity.java
+++ src/main/java/cn/nukkit/entity/Entity.java
@@ -135,6 +135,7 @@ public abstract class Entity extends Location implements Metadatable {
     public static final int DATA_RIDER_ROTATION_LOCKED = 57; //byte
     public static final int DATA_RIDER_MAX_ROTATION = 58; //float
     public static final int DATA_RIDER_MIN_ROTATION = 59; //float
+    public static final int DATA_RIDER_ROTATION_OFFSET = 60;
     public static final int DATA_AREA_EFFECT_CLOUD_RADIUS = 60; //float
     public static final int DATA_AREA_EFFECT_CLOUD_WAITING = 61; //int
     public static final int DATA_AREA_EFFECT_CLOUD_PARTICLE_ID = 62; //int
@@ -167,10 +168,10 @@ public abstract class Entity extends Location implements Metadatable {
     public static final int DATA_LIMITED_LIFE = 77; // int
     public static final int DATA_ARMOR_STAND_POSE_INDEX = 78; // int
     public static final int DATA_ENDER_CRYSTAL_TIME_OFFSET = 79; // int
-    public static final int DATA_ALWAYS_SHOW_NAMETAG = 80; // byte
+    public static final int DATA_ALWAYS_SHOW_NAMETAG = 81; // byte
     public static final int DATA_COLOR_2 = 81; // byte
     @Since("1.2.0.0-PN") public static final int DATA_NAME_AUTHOR = 82;
-    public static final int DATA_SCORE_TAG = 83; //String
+    public static final int DATA_SCORE_TAG = 84; //String
     public static final int DATA_BALLOON_ATTACHED_ENTITY = 84; // long
     public static final int DATA_PUFFERFISH_SIZE = 85;
     @Since("1.2.0.0-PN") public static final int DATA_BUBBLE_TIME = 86;
@@ -252,6 +253,8 @@ public abstract class Entity extends Location implements Metadatable {
     @Since("1.3.0.0-PN") public static final int DATA_HITBOX = 117;
     @Since("1.3.0.0-PN") public static final int DATA_IS_BUOYANT = 118;
     @Since("1.3.0.0-PN") public static final int DATA_BUOYANCY_DATA = 119;
+    @Since("1.4.0.0-PN")public static final int DATA_FREEZING_EFFECT_STRENGTH = 120;
+    @Since("1.4.0.0-PN")public static final int DATA_GOAT_HORN_COUNT  = 122;
 
     // Flags
     public static final int DATA_FLAG_ONFIRE = 0;
@@ -380,6 +383,7 @@ public abstract class Entity extends Location implements Metadatable {
     @Since("1.2.0.0-PN") public static final int DATA_FLAG_CELEBRATING = 92;
     @Since("1.3.0.0-PN") public static final int DATA_FLAG_ADMIRING = 93;
     @Since("1.3.0.0-PN") public static final int DATA_FLAG_CELEBRATING_SPECIAL = 94;
+    @Since("1.4.0.0-PN") public static final int DATA_FLAG_RAM_ATTACK  = 96;
 
     public static long entityCount = 1;
 

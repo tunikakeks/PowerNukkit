807dd95b25cc0f2e92db1b2b0122914977c85b45
Tobias CMC <73995049+PleaseInsertNameHere@users.noreply.github.com>
2021-07-02 17:28:29 +0200

    GlowBerries: Add food

--- src/main/java/cn/nukkit/item/ItemGlowBerries.java
+++ src/main/java/cn/nukkit/item/ItemGlowBerries.java
@@ -1,6 +1,10 @@
 package cn.nukkit.item;
 
+import cn.nukkit.Player;
 import cn.nukkit.block.BlockCaveVines;
+import cn.nukkit.event.player.PlayerItemConsumeEvent;
+import cn.nukkit.item.food.Food;
+import cn.nukkit.math.Vector3;
 
 /**
  * @author PleaseInsertNameHere
@@ -15,5 +19,32 @@ public class ItemGlowBerries extends StringItem {
         super(MinecraftItemID.GLOW_BERRIES.getNamespacedId(), "Glow Berries");
         this.block = new BlockCaveVines();
         this.count = count;
+    }
+
+    @Override
+    public boolean onClickAir(Player player, Vector3 directionVector) {
+        if (player.getFoodData().getLevel() < player.getFoodData().getMaxLevel() || player.isCreative()) {
+            return true;
+        }
+        player.getFoodData().sendFoodLevel();
+        return false;
+    }
+
+    @Override
+    public boolean onUse(Player player, int ticksUsed) {
+        PlayerItemConsumeEvent consumeEvent = new PlayerItemConsumeEvent(player, this);
+
+        player.getServer().getPluginManager().callEvent(consumeEvent);
+        if (consumeEvent.isCancelled()) {
+            player.getInventory().sendContents(player);
+            return false;
+        }
+
+        Food food = Food.glow_berries;
+        if ((player.isAdventure() || player.isSurvival()) && food.eatenBy(player)) {
+            --this.count;
+            player.getInventory().setItemInHand(this);
+        }
+        return true;
     }
 }

c5e84475427c86923b19729ff98ea75c9b446146
Tobias CMC <73995049+PleaseInsertNameHere@users.noreply.github.com>
2021-07-04 18:49:00 +0200

    Fix Bonemeal

--- src/main/java/cn/nukkit/item/ItemDye.java
+++ src/main/java/cn/nukkit/item/ItemDye.java
@@ -45,6 +45,8 @@ public class ItemDye extends Item {
     @Deprecated
     public static final int BLACK = DyeColor.BLACK.getDyeData();
 
+    private boolean isBoneMeal;
+
     public ItemDye() {
         this(0, 1);
     }
@@ -62,7 +64,9 @@ public class ItemDye extends Item {
     }
 
     public ItemDye(Integer meta, int amount) {
-        super(DYE, meta, amount, meta <= 15? DyeColor.getByDyeData(meta).getDyeName() : DyeColor.getByDyeData(meta).getName() + " Dye");
+        super(DYE, meta, amount, meta == 15 ? DyeColor.getByDyeData(meta).getDyeName() : DyeColor.getByDyeData(meta).getName() + " Dye");
+
+        this.isBoneMeal = (meta == 15);
 
         if (this.meta == DyeColor.BROWN.getDyeData()) {
             this.block = Block.get(BlockID.COCOA);
@@ -79,7 +83,7 @@ public class ItemDye extends Item {
     @PowerNukkitOnly
     @Override
     public boolean isFertilizer() {
-        return getId() == DYE && getDyeColor().equals(DyeColor.WHITE);
+        return getId() == DYE && getDyeColor().equals(DyeColor.WHITE) && isBoneMeal;
     }
     
     @Since("1.4.0.0-PN")

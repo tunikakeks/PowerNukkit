2ac30ee0110acfe64616269f5eb371d9307f14f8
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-07-15 15:36:11 +0900

    Improve

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorSpring.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorSpring.java
@@ -1,28 +1,29 @@
 package cn.nukkit.level.generator.populator.impl;
 
-import cn.nukkit.block.Block;
 import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
+import cn.nukkit.math.Vector3;
 
-import static cn.nukkit.block.BlockID.AIR;
-import static cn.nukkit.block.BlockID.STONE;
+import java.util.List;
 
 /**
  * @author GoodLucky777
  */
 public class PopulatorSpring extends Populator {
 
-    private final BlockState blockState;
+    private final BlockState state;
+    private final List<BlockState> surroundState;
     private final int tries;
     private final int minHeight;
     private final int maxHeight;
     
-    public PopulatorSpring(Block block, int tries, int minHeight, int maxHeight) {
-        this.blockState = block.getCurrentState();
+    public PopulatorSpring(BlockState state, List<BlockState> surroundState, int tries, int minHeight, int maxHeight) {
+        this.state = state;
+        this.surroundState = surroundState;
         this.tries = tries;
         this.minHeight = minHeight;
         this.maxHeight = maxHeight;
@@ -32,30 +33,27 @@ public class PopulatorSpring extends Populator {
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         int sourceX = chunkX << 4;
         int sourceZ = chunkZ << 4;
-        int endX = sourceX + 15;
-        int endZ = sourceZ + 15;
         
         for (int i = 0; i < tries; i++) {
-            int x = NukkitMath.randomRange(random, sourceX, endX);
-            int z = NukkitMath.randomRange(random, sourceZ, endZ);
+            int x = sourceX + random.nextBoundedInt(16);
+            int z = sourceZ + random.nextBoundedInt(16);
             int y = NukkitMath.randomRange(random, minHeight, maxHeight);
             
-            int replace = level.getBlockIdAt(x, y, z);
-            if (!(replace == AIR || replace == STONE)) {
+            if (!(level.getBlockStateAt(x, y, z).equals(BlockState.AIR) || surroundState.contains(level.getBlockStateAt(x, y, z)))) {
                 continue;
             }
             
-            if (level.getBlockIdAt(x, y - 1, z) != STONE || level.getBlockIdAt(x, y + 1, z) != STONE) {
+            if (!(surroundState.contains(level.getBlockStateAt(x, y - 1, z)) || surroundState.contains(level.getBlockStateAt(x, y + 1, z)))) {
                 continue;
             }
             
-            int stoneCount = 0;
-            if (level.getBlockIdAt(x + 1, y, z) == STONE) stoneCount++;
-            if (level.getBlockIdAt(x - 1, y, z) == STONE) stoneCount++;
-            if (level.getBlockIdAt(x, y, z + 1) == STONE) stoneCount++;
-            if (level.getBlockIdAt(x, y, z - 1) == STONE) stoneCount++;
+            int surroundCount = 0;
+            if (surroundState.contains(level.getBlockStateAt(x + 1, y, z))) surroundCount++;
+            if (surroundState.contains(level.getBlockStateAt(x - 1, y, z))) surroundCount++;
+            if (surroundState.contains(level.getBlockStateAt(x, y, z + 1))) surroundCount++;
+            if (surroundState.contains(level.getBlockStateAt(x, y, z - 1))) surroundCount++;
             
-            if (stoneCount != 3) {
+            if (surroundCount != 3) {
                 continue;
             }
             
@@ -69,8 +67,8 @@ public class PopulatorSpring extends Populator {
                 continue;
             }
             
-            level.setBlockStateAt(x, y, z, blockState);
-            // TODO: Tick the block immediately
+            level.setBlockStateAt(x, y, z, state);
+            level.getChunk(chunkX, chunkZ).getProvider().getLevel().scheduleUpdate(state.getBlock(), new Vector3(x, y, z), 1, 0, false);
         }
     }
 }

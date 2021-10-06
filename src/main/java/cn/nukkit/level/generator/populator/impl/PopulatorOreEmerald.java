274cad5183877be5f273c29d0cde25c4e79e422e
Tobias123567 <tobiasgotzl@gmail.com>
2021-10-06 20:45:02 +0200

    spawn deepslate emerald ores when tried to spawn in deepslate or tuff

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
@@ -17,19 +17,24 @@ import cn.nukkit.math.NukkitRandom;
 public class PopulatorOreEmerald extends Populator {
 
     private static final BlockState STATE_STONE = BlockState.of(STONE);
+    private static final BlockState STATE_DEEPSLATE = BlockState.of(DEEPSLATE);
+    private static final BlockState STATE_TUFF = BlockState.of(TUFF);
     private static final BlockState STATE_EMERALD_ORE = BlockState.of(EMERALD_ORE);
-    
+    private static final BlockState STATE_DEEPSLATE_EMERALD_ORE = BlockState.of(EMERALD_ORE);
+
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         for (int i = 0; i < 11; i++) {
             int x = random.nextBoundedInt(16);
             int z = random.nextBoundedInt(16);
             int y = NukkitMath.randomRange(random, 0, 32);
-            
-            if (chunk.getBlockState(x, y, z) != STATE_STONE) {
+
+            BlockState blockState = chunk.getBlockState(x, y, z);
+            if (blockState != STATE_STONE && blockState != STATE_DEEPSLATE && blockState != STATE_TUFF) {
                 continue;
             }
-            chunk.setBlockState(x, y, z, STATE_EMERALD_ORE);
+
+            chunk.setBlockState(x, y, z, blockState == STATE_DEEPSLATE || blockState == STATE_TUFF ? STATE_DEEPSLATE_EMERALD_ORE : STATE_EMERALD_ORE);
         }
     }
 }

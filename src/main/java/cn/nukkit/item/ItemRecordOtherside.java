d73e451ff2636ba00dbb2ce8cbf722df07829e5d
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2021-12-12 15:25:17 +0100

    Update Annotations

--- src/main/java/cn/nukkit/item/ItemRecordOtherside.java
+++ src/main/java/cn/nukkit/item/ItemRecordOtherside.java
@@ -2,23 +2,20 @@ package cn.nukkit.item;
 
 import cn.nukkit.api.Since;
 
-/**
- * @author PetteriM1
- */
-@Since("1.4.0.0-PN")
+@Since("FUTURE")
 public class ItemRecordOtherside extends ItemRecord {
 
-    @Since("1.4.0.0-PN")
+    @Since("FUTURE")
     public ItemRecordOtherside() {
         this(0, 1);
     }
 
-    @Since("1.4.0.0-PN")
+    @Since("FUTURE")
     public ItemRecordOtherside(Integer meta) {
         this(meta, 1);
     }
 
-    @Since("1.4.0.0-PN")
+    @Since("FUTURE")
     public ItemRecordOtherside(Integer meta, int count) {
         super(RECORD_OTHERSIDE, meta, count);
     }

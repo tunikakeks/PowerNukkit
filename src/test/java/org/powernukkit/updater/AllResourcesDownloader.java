6dcef16d6608245d8f0bf8d057ecf1297a5981a4
joserobjr <joserobjr@powernukkit.org>
2022-04-20 10:58:38 -0300

    Update all resources to MC 1.18.30

--- src/test/java/org/powernukkit/updater/AllResourcesDownloader.java
+++ src/test/java/org/powernukkit/updater/AllResourcesDownloader.java
@@ -1,19 +1,40 @@
 package org.powernukkit.updater;
 
+import cn.nukkit.nbt.NBTIO;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.ByteOrder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author joserobjr
  * @since 2021-10-24
  */
 public class AllResourcesDownloader {
+    private static final String CANONICAL_BLOCK_STATES_PATH = "src/main/resources/canonical_block_states.nbt";
+    private static final String CANONICAL_BLOCK_STATES_URL =
+            "https://github.com/pmmp/BedrockData/raw/master/canonical_block_states.nbt"
+            //"https://github.com/CloudburstMC/Data/raw/master/block_palette.nbt"
+            ;
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private static final Optional<String> USE_NUKKIT_RUNTIME_BLOCK_STATES_DAT =
+            //Optional.of("https://github.com/CloudburstMC/Nukkit/raw/v1.18.30/src/main/resources/runtime_block_states.dat")
+            Optional.empty()
+            ;
+
     public static void main(String[] args) {
         /*
         Pre-requisites:
@@ -34,10 +55,34 @@ public class AllResourcesDownloader {
     }
 
     private void downloadResources() {
-        download("https://github.com/pmmp/BedrockData/raw/master/canonical_block_states.nbt",
-                "src/main/resources/canonical_block_states.nbt");
+        createCanonicalBlockStatesNbt();
         download("https://github.com/pmmp/BedrockData/raw/master/required_item_list.json",
                 "src/test/resources/org/powernukkit/updater/dumps/pmmp/required_item_list.json");
+    }
+
+    @SneakyThrows
+    private void createCanonicalBlockStatesNbt() {
+        if (!USE_NUKKIT_RUNTIME_BLOCK_STATES_DAT.isPresent()) {
+            download(CANONICAL_BLOCK_STATES_URL, CANONICAL_BLOCK_STATES_PATH);
+            return;
+        }
+        Path runtimeBlockStatesFile = Files.createTempFile("runtime_block_states", ".dat");
+        try {
+            download(USE_NUKKIT_RUNTIME_BLOCK_STATES_DAT.get(), runtimeBlockStatesFile.toAbsolutePath().toString());
+            try (InputStream fileInputStream = Files.newInputStream(runtimeBlockStatesFile);
+                 BufferedInputStream inputStream = new BufferedInputStream(fileInputStream)
+            ) {
+                //noinspection unchecked
+                List<CompoundTag> tags = ((ListTag<CompoundTag>) NBTIO.readTag(inputStream, ByteOrder.BIG_ENDIAN, false)).getAll();
+                tags.forEach(tag-> tag.remove("id").remove("data"));
+                tags.sort(Comparator.comparingInt(tag -> tag.getInt("runtimeId")));
+                Assertions.assertEquals(tags.size() - 1, tags.get(tags.size() - 1).getInt("runtimeId"));
+                System.out.println(tags);
+                throw new InternalError(); // Aborting this idea. Cloudburst Nukkit's dat file lacks A LOT of states.
+            }
+        } finally {
+            Files.deleteIfExists(runtimeBlockStatesFile);
+        }
     }
 
     private void copyProxyPassResources(String pathProxyPassData) {

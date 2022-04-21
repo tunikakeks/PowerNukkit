a0a18635c527bbdef8769f2b16eb7523aae73e33
Tobias123567 <tobiasgotzl@gmail.com>
2022-04-21 02:54:37 +0200

    do not open double chest when block is above one half of the double chest

--- src/main/java/cn/nukkit/block/BlockChest.java
+++ src/main/java/cn/nukkit/block/BlockChest.java
@@ -197,13 +197,21 @@ public class BlockChest extends BlockTransparentMeta implements Faceable, BlockE
         if (player == null) {
             return false;
         }
-        
-        Block top = up();
-        if (!top.isTransparent() && !player.isSpectator()) {
-            return false;
-        }
 
         BlockEntityChest chest = getOrCreateBlockEntity();
+        if (!player.isSpectator()) {
+            Block top = up();
+            if (!top.isTransparent()) {
+                return false;
+            }
+            if (chest.isPaired()) {
+                top = chest.getPair().getSide(BlockFace.UP).getLevelBlock();
+                if (!top.isTransparent()) {
+                    return false;
+                }
+            }
+        }
+
         if (chest.namedTag.contains("Lock") && chest.namedTag.get("Lock") instanceof StringTag 
                 && !chest.namedTag.getString("Lock").equals(item.getCustomName())) {
             return false;

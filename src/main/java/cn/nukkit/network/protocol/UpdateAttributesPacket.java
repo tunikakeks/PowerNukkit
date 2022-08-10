818ce735d614bc61d59fab1e4467cc34b9161a4f
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2022-08-10 11:36:11 +0200

    Fix wrong position of Attribute

--- src/main/java/cn/nukkit/network/protocol/UpdateAttributesPacket.java
+++ src/main/java/cn/nukkit/network/protocol/UpdateAttributesPacket.java
@@ -44,10 +44,10 @@ public class UpdateAttributesPacket extends DataPacket {
                 this.putLFloat(entry.getValue());
                 this.putLFloat(entry.getDefaultValue());
                 this.putString(entry.getName());
+                this.putUnsignedVarInt(0);
             }
         }
         this.putUnsignedVarInt(this.frame);
-        this.putUnsignedVarInt(0);
     }
 
 }

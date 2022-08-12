aaf755910aa94d169fc279ce911e5c6375de8b63
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2022-08-12 19:37:02 +0200

    EntityMooshroom: Drop steak when lit

--- src/main/java/cn/nukkit/entity/passive/EntityMooshroom.java
+++ src/main/java/cn/nukkit/entity/passive/EntityMooshroom.java
@@ -42,7 +42,7 @@ public class EntityMooshroom extends EntityAnimal {
 
     @Override
     public Item[] getDrops() {
-        return new Item[]{Item.get(Item.LEATHER), Item.get(Item.RAW_BEEF)};
+        return new Item[]{Item.get(Item.LEATHER), Item.get(((this.isOnFire()) ? Item.COOKED_BEEF : Item.RAW_BEEF))};
     }
 
     @Override

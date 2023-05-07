fb3216e6d4921b2d4d36e1b548995cbdb3b80865
KeksDev <54219265+tunikakeks@users.noreply.github.com>
2023-05-07 13:54:10 +0200

    5

--- src/main/java/cn/nukkit/blockstate/BlockStateRegistry.java
+++ src/main/java/cn/nukkit/blockstate/BlockStateRegistry.java
@@ -203,7 +203,7 @@ public class BlockStateRegistry {
                 }
             } else */
             
-            for(var entry : cfg.config.entrySet()) {
+            for(var entry : cfg.getSections()) {
                 var subMap = entry.getValue().getAsJsonObject();
                 if (subMap.has(name)) {
                     // we have a submap, so we need to register all the submap entries
